26c5fd301acf1e728a9d9995ecf42d32f11e9ed5
funcman <hyq1986@gmail.com>
2014-02-20 00:50:01 +0800

    basic interface

---
+++ fstr.h
@@ -0,0 +1,14 @@
+#ifndef FSTR_H
+#define FSTR_H
+
+struct fStr;
+
+struct fStr*    fstr_create(char* cstr);
+void            fstr_release(struct fStr* fstr);
+struct fStr*    fstr_copy(struct fStr* fstr);
+unsigned int    fstr_len(struct fStr* fstr);
+struct fStr*    fstr_append(struct fStr* fstr, char* cstr, unsigned int len);
+char*           fstr_to_cstr(struct fStr* fstr);
+
+#endif//FSTR_H
+
